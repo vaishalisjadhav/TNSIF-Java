7b86f210ee15f7113cae4155ce7a0496f49e2575
vaishalisjadhav <vaishalijadhav9777@gmail.com>
2025-01-29 14:37:38 +0530

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a=10;
+        long l=20;
+        char b='A';
+        String name="vaishali";
+        float r= 0.6f;
+        double d= 0.9999999999999;
+        boolean g=true;
+        byte j='q';
+        System.out.println(a);
+        System.out.println(l);
+        System.out.println(b);
+        System.out.println(name);
+        System.out.println(r);
+        System.out.println(d);
+        System.out.println(g);
+        System.out.println(j);
+    }
+}

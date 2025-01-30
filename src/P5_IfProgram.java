282b796f420e93bdbac0f986a8400ebf2f02145e
vaishalisjadhav <vaishalijadhav9777@gmail.com>
2025-01-30 16:02:50 +0530

    If Program

---
+++ src/P5_IfProgram.java
@@ -0,0 +1,8 @@
+public class P5_IfProgram {
+    public static void main(String[] args) {
+        int x=5;
+        if(x==5){
+            System.out.println("Value of x is 5");
+        }
+    }
+}

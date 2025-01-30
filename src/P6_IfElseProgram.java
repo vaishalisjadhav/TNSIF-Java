00aa88ca7a3c3616fb44b00266474d2d4d5a9f4e
vaishalisjadhav <vaishalijadhav9777@gmail.com>
2025-01-30 16:05:23 +0530

    IfElse Program

---
+++ src/P6_IfElseProgram.java
@@ -0,0 +1,10 @@
+public class P6_IfElseProgram {
+    public static void main(String[] args) {
+        int age = 21;
+        if(age>18){
+            System.out.println("You are eligible for voting");
+        } else {
+            System.out.println("You are not eligible for voting");
+        }
+    }
+}

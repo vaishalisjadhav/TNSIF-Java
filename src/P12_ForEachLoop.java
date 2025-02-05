dd8d17bceb502687427cf085ee82d5b5f2067003
vaishalisjadhav <vaishalijadhav9777@gmail.com>
2025-02-05 14:44:58 +0530

    For Each loop

---
+++ src/P12_ForEachLoop.java
@@ -0,0 +1,12 @@
+public class P12_ForEachLoop {
+    public static void main(String[] args) {
+        int a[] = {10, 20, 30, 40, 50};
+        for(int i:a){
+            System.out.println(i);
+        }
+        char ch[]= {'j', 'a', 'v', 'a'};
+        for(char c:ch) {
+            System.out.print(c);
+        }
+    }
+}

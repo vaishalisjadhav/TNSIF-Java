cc5b0edf4eb10d037d8a4b8d1a74ca9b9d7e63ac
vaishalisjadhav <vaishalijadhav9777@gmail.com>
2025-02-05 14:35:00 +0530

    While loop

---
+++ src/P10_WhileLoop.java
@@ -0,0 +1,9 @@
+public class P10_WhileLoop {
+    public static void main(String[] args) {
+        int i = 95;
+        while(i<=100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}

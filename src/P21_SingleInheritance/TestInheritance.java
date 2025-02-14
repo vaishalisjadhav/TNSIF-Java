ee1cc445b7798d079cbfc2c7a9e11dda19b544e3
vaishalisjadhav <vaishalijadhav9777@gmail.com>
2025-02-14 14:18:35 +0530

    Code Added

---
+++ src/P21_SingleInheritance/TestInheritance.java
@@ -0,0 +1,8 @@
+package P21_SingleInheritance;
+
+public class TestInheritance {
+    public static void main(String[] args) {
+        Student s1=new Student("Vaishali",1234567890,"Solapur",5,"Keystone");
+        System.out.println(s1);
+    }
+}
